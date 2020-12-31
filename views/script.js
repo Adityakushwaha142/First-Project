f39483b660a03dafc4f80bccd626cd6fa5273df3
Aditya Kushwaha <kushwahaaditya251@gmail.com>
2020-12-31 20:02:22 +0530

    changeing in css

--- views/script.js
+++ views/script.js
@@ -1,15 +1,24 @@
+/* deliver  schedule */
+
 $(()=>{
-    $("#deliver").click(()=>{
-        $('#deliver').prop('disabled', false);
-$('#schedule').prop('disabled', true);
+    $(document).on('click', "#btn1", function() {
+
+        if($("#btn2").hasClass("border border-info")){
+            $("#btn2").removeClass("border border-info")
+            $("this").addClass("border border-info") ; 
+        }
+    })
+
+    console.log("hello world") ; 
+
+    console.log("hello world") ; console.log("hello world") ; console.log("hello world") ; console.log("hello world") ; console.log("hello world") ; console.log("hello world") ; 
+    $(document).on('click', "#btn2", function() {
+
+        if($("#btn1").hasClass("border border-info")){
+            $("#btn1").removeClass("border border-info")
+            $("#this").addClass("border border-info") ; 
+        }
     })
 
 
-
-    $(document).ready(function(){ 
-        $('#deliver').click(function() { 
-            $('#deliver').toggleClass('active');
-            $('#schedule').toggleClass('active');
-        });
-        });
 })
